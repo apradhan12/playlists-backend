3c5fae970928c46d33cee7d5f9a5d02ea4dbe936
Aaron Pradhan <12a.pradhan@gmail.com>
2021-05-04 22:16:51 -0400

    Partially implement getSongRequests

--- src/routes/requests.ts
+++ src/routes/requests.ts
@@ -1,15 +1,59 @@
 import { Request, Response } from 'express';
 import StatusCodes from "http-status-codes";
 import {db} from "../common/database.js";
+import {SongRequestList} from "../common/apiTypes.js";
+import assert from "assert";
+import * as dbTypes from "../common/dbTypes.js";
 
-const { OK } = StatusCodes;
+const { OK, BAD_REQUEST } = StatusCodes;
 
 export async function getSongRequests(req: Request, res: Response) {
-    const rows = await db.select().from("song_requests")
+    console.log("getting song requests");
+    // validate Authorization header
+    const authHeader = req.header("Authorization");
+    if (authHeader === undefined) {
+        return res.status(BAD_REQUEST).json({}); // todo
+    }
+    const regexMatch = authHeader.match(/^Bearer (.*)$/);
+    if (regexMatch === null) {
+        return res.status(BAD_REQUEST).json({}); // todo
+    }
+    const accessToken = regexMatch[1];
+
+    // get user from access token
+    const userIds = await db.select("user_id").from("users")
         .where({
-            playlist_id: req.params.playlistId
+            access_token: accessToken
         });
-    return res.status(OK).json(rows);
+    assert(userIds.length === 1);
+    const userId = userIds[0];
+    // todo: error handle if more than one user ID
+
+    const adminRows = await db.select().from("administrators")
+        .where({
+            playlist_id: req.params.playlistId,
+            user_id: userId
+        });
+    assert(adminRows.length <= 1);
+
+    const isAdmin = adminRows.length === 1;
+
+    // todo: request these concurrently
+    const addRequests: dbTypes.SongRequest[] = await db.select().from("song_requests")
+        .where({
+            playlist_id: req.params.playlistId,
+            request_type: dbTypes.RequestType.Add
+        });
+
+    const removeRequests: dbTypes.SongRequest[] = await db.select().from("song_requests")
+        .where({
+            playlist_id: req.params.playlistId,
+            request_type: dbTypes.RequestType.Remove
+        });
+
+    // TODO: look at https://api.spotify.com/v1/tracks
+
+    return res.status(OK).json(addRequests);
 }
 
 export async function requestSongs(req: Request, res: Response) {
