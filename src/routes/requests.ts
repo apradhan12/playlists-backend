b57612d3b2d8e56ef87658e61f8c82e4401babc6
Aaron Pradhan <12a.pradhan@gmail.com>
2021-05-11 17:06:28 -0400

    Implement add song requests

--- src/routes/requests.ts
+++ src/routes/requests.ts
@@ -2,13 +2,15 @@ import {Request, Response} from 'express';
 import StatusCodes from "http-status-codes";
 import {db} from "../common/database.js";
 import * as apiTypes from "../common/apiTypes.js";
+import * as commonTypes from "../common/commonTypes.js";
 import assert from "assert";
 import * as dbTypes from "../common/dbTypes.js";
 import got, {Response as GotResponse} from "got";
 import {RequestStatus} from "../common/commonTypes.js";
 import {Unauthorized} from "../common/errors.js";
+import _ from "lodash";
 
-const { OK, BAD_REQUEST } = StatusCodes;
+const { OK, UNPROCESSABLE_ENTITY } = StatusCodes;
 
 interface DBSongRequest {
     request_id: number;
@@ -94,27 +96,18 @@ WHERE r.playlist_id = 'playlist1' AND r.status = 'pending'
 GROUP BY r.request_id;
      */
 
-    const tracksResponse: GotResponse<any> = await got("https://api.spotify.com/v1/tracks", {
-        headers: {
-            Authorization: authHeader
-        },
-        searchParams: {
-            ids: songRequests.map(request => request.song_id).join(","),
-            market: "from_token"
-        },
-        responseType: "json"
-    });
+    const tracks = await getTracks(authHeader, songRequests.map(request => request.song_id));
     // todo: handle error
     // todo: add Spotify types?
 
-    assert(songRequests.length === tracksResponse.body.tracks.length);
+    assert(songRequests.length === tracks.length);
 
     const addRequests = [];
     const removeRequests = [];
 
     for (let i = 0; i < songRequests.length; i++) {
         const songRequest = songRequests[i];
-        const track = tracksResponse.body.tracks[i];
+        const track = tracks[i];
         const hasYourVote = requestHasYourVote[i];
         const apiSongRequest = {
             requestId: songRequest.request_id,
@@ -144,14 +137,110 @@ GROUP BY r.request_id;
     return res.status(OK).json(songRequestList);
 }
 
-export async function requestSongs(req: Request, res: Response) {
-    const userId = await getUserId(req.header("Authorization"));
-    // of the songsToAdd, check which ones are in the playlist already
-    // of the songsToRemove, check
-    return res.status(OK).json({
-        songsAlreadyInPlaylist: [],
-        songsNotInPlaylist: []
+async function getTracks(authHeader: string | undefined, songIds: string[]): Promise<any[]> {
+    const tracksResponse: GotResponse<any> = await got("https://api.spotify.com/v1/tracks", {
+        headers: {
+            Authorization: authHeader
+        },
+        searchParams: {
+            ids: songIds.join(","),
+            market: "from_token"
+        },
+        responseType: "json"
     });
+    return tracksResponse.body.tracks;
+}
+
+export async function requestSongs(req: Request, res: Response) {
+    const authHeader = req.header("Authorization");
+    const userId = await getUserId(authHeader);
+
+    const body: apiTypes.SongUpdateList = req.body;
+    // todo: is this "if" needed?
+    if (body.songsToAdd.length === 0 && body.songsToRemove.length === 0) {
+        return res.status(OK).json({songsAlreadyInPlaylist: [], songsNotInPlaylist: []});
+    }
+
+    // enforce that all songs are valid
+    const allIds = body.songsToAdd.concat(body.songsToRemove);
+    const allTracks = await getTracks(authHeader, allIds);
+    const nullTracks = Array.from(allTracks.entries()).filter(([, track]) => track === null);
+    if (nullTracks.length > 0) {
+        return res.status(UNPROCESSABLE_ENTITY).json({
+            invalidSongIds: nullTracks.map(([i, ]) => allIds[i])
+        });
+    }
+
+    // get the songs in the playlist
+    const playlistSongIds: string[] = await getSongsInPlaylist(req.params.playlistId, authHeader);
+
+    // compare requested songs to those in playlist
+    console.log(`request body: ${JSON.stringify(body)}`);
+    const songsToAdd = _.uniq(body.songsToAdd);
+    const songsToRemove = _.uniq(body.songsToRemove);
+    const [songsAlreadyInPlaylist, filteredSongsToAdd] = _.partition(songsToAdd, songId => playlistSongIds.includes(songId));
+    const [filteredSongsToRemove, songsNotInPlaylist] = _.partition(songsToRemove, songId => playlistSongIds.includes(songId));
+
+    const songIdToRequest = (requestType: dbTypes.RequestType) => (songId: string) => ([
+        songId,
+        requestType
+    ]);
+    const allRequests = filteredSongsToAdd.map(songIdToRequest(dbTypes.RequestType.Add))
+        .concat(filteredSongsToRemove.map(songIdToRequest(dbTypes.RequestType.Remove)));
+
+    console.log("now i'm here");
+    const existingRequests = await db.select(["request_id", "song_id", "request_type"]).from("song_requests")
+        .whereIn(["song_id", "request_type"], allRequests);
+    console.log("now I'm there");
+
+    const newRequests = allRequests.filter(request => !existingRequests.some(existingRequest => existingRequest.song_id === request[0] && existingRequest.request_type === request[1]));
+    // todo: enforce that song IDs are valid Spotify tracks
+    const existingRequestsVotedFor = await db.select(["request_id"]).from("request_votes")
+        .whereIn("request_id", existingRequests.map(request => request.request_id))
+        .andWhere("user_id", userId);
+    console.log("now I'm there2");
+    const existingRequestsNotVotedFor = existingRequests.filter(request => !existingRequestsVotedFor.some(requestVotedFor => requestVotedFor.request_id === request.request_id));
+    // todo: unit test
+
+    if (existingRequestsNotVotedFor.length > 0) {
+        await db("request_votes").insert(existingRequestsNotVotedFor.map(request => ({request_id: request.request_id, user_id: userId})));
+    }
+    console.log("now I'm there3");
+    if (newRequests.length > 0) {
+        const requestIdList = await db("song_requests").insert(newRequests.map(request => ({
+            playlist_id: req.params.playlistId,
+            request_type: request[1],
+            song_id: request[0],
+            status: commonTypes.RequestStatus.Pending,
+        })));
+        const firstRequestId = requestIdList[0];
+        await db("request_votes").insert(_.range(firstRequestId, firstRequestId + newRequests.length).map(requestId => ({
+            request_id: requestId,
+            user_id: userId
+        })));
+    }
+    console.log("now I'm there4");
+
+    // of the songsToAdd, check which ones are in the playlist already
+    // of the songsToRemove, check which ones are not in the playlist
+    return res.status(OK).json({
+        songsAlreadyInPlaylist: songsAlreadyInPlaylist,
+        songsNotInPlaylist: songsNotInPlaylist
+    });
+}
+
+async function getSongsInPlaylist(playlistId: string, authHeader: string | undefined): Promise<string[]> {
+    const playlistTracksResponse: GotResponse<any> = await got(`https://api.spotify.com/v1/playlists/${playlistId}`, {
+        headers: {
+            Authorization: authHeader
+        },
+        searchParams: {
+            fields: "tracks.items(track(id))",
+            market: "from_token"
+        },
+        responseType: "json"
+    });
+    return playlistTracksResponse.body.tracks.items.map((item: any) => item.track.id);
 }
 
 export async function updateRequestStatus(req: Request, res: Response) {
